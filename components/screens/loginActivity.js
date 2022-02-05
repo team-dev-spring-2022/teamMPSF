90bd9d1267a0cd0060aa317aa7b232507ca30e01
Deslifer <kolmingstar@mail.ru>
2022-02-05 11:32:09 +0900

    #6 Страница входа создана

---
+++ components/screens/loginActivity.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+
+const LoginActivity = () => {
+  return (
+    // @todo #6 Добавить кнопки login и sign up
+  );
+};
+
+export default LoginActivity;
