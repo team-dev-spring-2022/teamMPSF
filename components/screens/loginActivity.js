c370ebf9f8a3bd49b94a013f4ec93513cf26a3e4
Deslifer <kolmingstar@mail.ru>
2022-02-05 11:37:29 +0900

    #6 Добавил страницу входа

--- components/screens/loginActivity.js
+++ components/screens/loginActivity.js
@@ -7,11 +7,8 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-
 const LoginActivity = () => {
-  return (
-    // @todo #6 Добавить кнопки login и sign up
-  );
+  // @todo #6 Добавить кнопки login и sign up
 };
 
 export default LoginActivity;
