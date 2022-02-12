1d88f04f4fa8e0470d8a4c61de8a7b7c6f47dd29
Saykolyan <saykolyan1996@gmail.com>
2022-02-13 00:25:44 +0900

    #27 подключил Stack Navigator к приложению

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import BottomTabNavigator from './components/routers/BottomTabNavigator';
+import StackNavigator from './components/routers/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      <StackNavigator />
     </NavigationContainer>
   );
 };
