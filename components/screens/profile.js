84c8734f085222bc3254bb84c5979cc31d5f5320
Deslifer <33741283+Deslifer@users.noreply.github.com>
2022-02-15 20:13:57 +0900

    Revert "#48 Сделал передачу данных пользователя через Async Storage"

--- components/screens/profile.js
+++ components/screens/profile.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
   View,
   Text,
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const styles = StyleSheet.create({
   // @todo #35 Добавить стили для страницы профиля
@@ -14,19 +13,11 @@ export const styles = StyleSheet.create({
 });
 
 const Profile = () => {
-  const [login, setLogin] = useState();
-  const [password, setPassword] = useState();
-
-  const dataFromStore = async () => {
-    setLogin((await AsyncStorage.getItem('login')) || null);
-    setPassword((await AsyncStorage.getItem('password')) || null);
-  };
-  dataFromStore();
   // @todo #35 Доделать страницу профиля в будущем
   return (
     <View>
       <Text>Имя:</Text>
-      <TextInput placeholder="Имя" value={login} />
+      <TextInput placeholder="Имя" />
       <Text>Фамилия:</Text>
       <TextInput placeholder="Фамилия" />
       <Text>Адрес почты</Text>
