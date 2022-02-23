f1eab899cc5e7fb40ea2a7529383824430ef16f9
Deslifer <kolmingstar@mail.ru>
2022-02-23 12:15:00 +0900

    #61 Создал сервер и подключил к приложению

---
+++ components/gqls/tasks/queries.js
@@ -0,0 +1,11 @@
+import {gql} from '@apollo/client';
+
+export const GET_TASKS = gql`
+  query tasks {
+    tasks {
+      id
+      title
+      description
+    }
+  }
+`;
---
+++ config.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://mpsf-server.herokuapp.com/';
