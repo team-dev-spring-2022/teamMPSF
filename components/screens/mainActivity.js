878b9d3fe6d5c821bee2145613863a8a73c66415
Saykolyan <saykolyan1996@gmail.com>
2022-02-26 22:04:40 +0900

    #65 Добавил получение тасков с сервера

--- components/screens/mainActivity.js
+++ components/screens/mainActivity.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {Text, View, ScrollView, StyleSheet} from 'react-native';
+import {GET_TASKS} from '../gqls/tasks/queries';
+import {useQuery} from '@apollo/client';
+
+const styles = StyleSheet.create({
+  scroll: {},
+  buttonText: {},
+});
 
 const MainActivity = () => {
   // @todo #29 Добавить элементы главного экрана
-  return <ScrollView />;
+  const {loading, error, data} = useQuery(GET_TASKS);
+  // @todo #29 Добавить элементы главного экрана
+  if (loading) {
+    return (
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.buttonText}>Loading</Text>
+      </ScrollView>
+    );
+  }
+  return (
+    <ScrollView style={styles.scroll}>
+      {data.tasks.map(item => {
+        return (
+          <View key={item.id} style={{alignSelf: 'center'}}>
+            <Text style={styles.buttonText}>
+              {item.title} {item.description}
+            </Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
 };
 
 export default MainActivity;
