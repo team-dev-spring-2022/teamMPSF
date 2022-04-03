7858bcb0b19085c08db5cf60e8d3bf179e7111a4
Saykolyan <saykolyan1996@gmail.com>
2022-04-03 22:19:25 +0900

    #103 добавил дату в мутации

--- components/gqls/tasks/mutations.js
+++ components/gqls/tasks/mutations.js
@@ -9,16 +9,35 @@ export const DTASK = gql`
 `;
 
 export const NTASK = gql`
-  mutation createTask($title: String!, $description: String!, $mail: String!) {
-    createTask(input: {title: $title, description: $description, mail: $mail}) {
+  mutation createTask(
+    $title: String!
+    $description: String!
+    $mail: String!
+    $date: Date!
+  ) {
+    createTask(
+      input: {
+        title: $title
+        description: $description
+        mail: $mail
+        date: $date
+      }
+    ) {
       id
     }
   }
 `;
 
 export const UTASK = gql`
-  mutation updateTask($id: String!, $title: String!, $description: String!) {
-    updateTask(input: {id: $id, title: $title, description: $description}) {
+  mutation updateTask(
+    $id: String!
+    $title: String!
+    $description: String!
+    $date: Date!
+  ) {
+    updateTask(
+      input: {id: $id, title: $title, description: $description, date: $date}
+    ) {
       id
     }
   }
--- components/gqls/tasks/queries.js
+++ components/gqls/tasks/queries.js
@@ -7,6 +7,7 @@ export const GET_TASKS = gql`
       title
       description
       mail
+      date
     }
   }
 `;
